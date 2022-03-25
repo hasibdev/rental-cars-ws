9f2314f21f4a318d6722ab9f09b72c845df8db57
Hasibur Rahman <hasib.active@gmail.com>
2022-03-25 23:32:47 +0600

    calender working

--- components/RangeCalender.vue
+++ components/RangeCalender.vue
@@ -3,15 +3,23 @@
       <div class="col-6">
          <p class="base-color">Pick-up date</p>
 
-         <div>
-            <DateRangePicker :dateRange="dateRange" :autoApply="true" />
+         <div class="calender-wrapper">
+            <DateRangePicker :dateRange="dateRange" :autoApply="true" :append-to-body="true">
+               <template #ranges>
+                  <span class="d-none"></span>
+               </template>
+            </DateRangePicker>
          </div>
       </div>
       <div class="col-6">
          <p class="base-color">Return date</p>
 
-         <div>
-            <DateRangePicker :dateRange="dateRange" :autoApply="true" />
+         <div class="calender-wrapper">
+            <DateRangePicker :dateRange="dateRange" :autoApply="true" :append-to-body="true">
+               <template #ranges>
+                  <span class="d-none"></span>
+               </template>
+            </DateRangePicker>
          </div>
 
       </div>
@@ -49,5 +57,58 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+.base-color {
+   color: #ff5f00;
+}
+
+.daterangepicker {
+   width: 100vw !important;
+   left: 0 !important;
+   transform: translate(0, 0) !important;
+   padding: 30px 0;
+   .calendars-container {
+      width: 100%;
+   }
+   .drp-calendar {
+      .calendar-table {
+         table {
+            thead {
+               th {
+                  &.month {
+                     font-size: 1.5rem;
+                     font-weight: 700;
+                     line-height: 1.17;
+                  }
+               }
+            }
+            tbody {
+               th {
+                  padding: 0.4375rem;
+                  display: table-cell;
+                  font-size: 0.875rem;
+                  font-weight: 500;
+                  line-height: 1.3;
+                  padding: 3.4% 0;
+                  text-align: center;
+                  text-transform: uppercase;
+               }
+               td {
+                  padding: 0.6875rem 0;
+                  font-size: 1.125rem;
+                  font-weight: 500;
+                  position: relative;
+                  text-align: center;
+                  vertical-align: middle;
+               }
+            }
+         }
+      }
+   }
+
+   // .daterangepicker .drp-calendar {
+   //    max-width: initial;
+   //    width: initial;
+   // }
+}
 </style>
